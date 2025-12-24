1e9172a81656b1d0dbb2f01617987182d9aae866
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-24 17:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let heading = document.getElementById("head");
 let button = document.getElementById("bt");
 
 button.addEventListener("click", () => {
-  heading.textContent = Number(heading.textContent) + 1;
 	alert(heading.textContent);
+  heading.textContent = Number(heading.textContent) + 1;
+	
 });
