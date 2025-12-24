32a94cc99ec469e33c20e578b78cce8baf49d497
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-24 17:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let heading = document.getElementById("head");
+let button = document.getElementById("bt");
+
+button.addEventListener("click", () => {
+  heading.textContent = Number(heading.textContent) + 1;
+	alert(heading.textContent);
+});
